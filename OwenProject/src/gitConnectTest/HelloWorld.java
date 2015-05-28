5e2025629d7ed70ac21de3e95243c3f72766e504
wowokorea <wowokorea@gmail.com>
2015-05-28 11:21:26 +0900

    added test code

--- OwenProject/src/gitConnectTest/HelloWorld.java
+++ OwenProject/src/gitConnectTest/HelloWorld.java
@@ -6,9 +6,27 @@ public class HelloWorld {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
-		System.out.println("Hello World");
+		long currentMiliSeconds = System.currentTimeMillis();
+		System.out.println(currentMiliSeconds);
+		
+		long totalSeconds = currentMiliSeconds / 1000;
+		System.out.println(totalSeconds);
+		
+		long currentSeconds = totalSeconds % 60;
+		System.out.println(currentSeconds);
+		
+		long totalMinute = totalSeconds / 60;
+		System.out.println(totalMinute);
+		
+		long currentMinute = totalMinute % 60;
+		System.out.println(currentMinute);
+		
+		long totalHours = currentMinute / 60;
+		System.out.println(totalHours);
+		
+		long currentHours = totalHours % 24;
+		System.out.println(currentHours);
 		
 	}
 
