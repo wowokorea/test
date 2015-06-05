102a1eabf029f14e5d59755a162df93b02467caa
wowokorea <wowokorea@gmail.com>
2015-06-05 14:08:54 +0900

    add test code

--- OwenProject/src/gitConnectTest/HelloWorld.java
+++ OwenProject/src/gitConnectTest/HelloWorld.java
@@ -7,6 +7,7 @@ public class HelloWorld {
 	 */
 	public static void main(String[] args) {
 
+		/*
 		long currentMiliSeconds = System.currentTimeMillis();
 		System.out.println(currentMiliSeconds);
 		
@@ -27,6 +28,45 @@ public class HelloWorld {
 		
 		long currentHours = totalHours % 24;
 		System.out.println(currentHours);
+		*/
+		
+		System.out.println("너랑은 상관없음1");
+		
+		for (int i = 0; i < 3; i++) {
+			System.out.println("----------------------" + i + "----------------------");
+			
+			try {
+				//throw new Exception();
+			} catch (Exception e) {
+				System.out.println("익셉션 발생");
+				
+				if (i < 3) {
+
+					try {
+						Thread.sleep(1);
+					} catch (Exception e2) {
+						System.out.println("스레드 슬립에서 익셉션 발생");
+						break;
+					}
+					
+					System.out.println("다시 돌아라");
+					
+					continue;
+				} else {
+					System.out.println("오류 계속나서 끝");
+				}
+				
+				System.out.println("로그 찍을거야");
+				return;
+				
+			}
+			
+			break;
+			
+		}
+		
+		System.out.println("너랑은 상관없음2");
+		System.out.println("너랑은 상관없음3");
 		
 	}
 
