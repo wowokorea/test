8f315a75a92f8b362e3f6072a63d46022d985243
wowokorea <wowokorea@gmail.com>
2015-06-29 15:37:06 +0900

    test

---
+++ OwenProject/src/gitConnectTest/StringTest.java
@@ -0,0 +1,12 @@
+package gitConnectTest;
+
+public class StringTest {
+	public static void main(String[] args) {
+		
+		String test = "123456";
+		
+		System.out.println(test.length());
+		System.out.println(test.substring(0, 3) + "-" + test.substring(3, test.length()));
+		
+	}
+}
---
+++ OwenProject/src/stringTest/ReplaceTest.java
@@ -0,0 +1,13 @@
+package stringTest;
+
+public class ReplaceTest {
+	
+	public static void main(String[] args) {
+	
+		String postNo = "123-456";
+		System.out.println(postNo);
+		
+		System.out.println(postNo.replace("-", ""));
+		
+	}
+}
