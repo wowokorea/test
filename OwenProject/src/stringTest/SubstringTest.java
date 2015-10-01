fd55eb074067d36da1aaf9aacf18efa36ba4f876
wowokorea <wowokorea@gmail.com>
2015-10-01 10:52:05 +0900

    test code

--- OwenProject/src/stringTest/SubstringTest.java
+++ OwenProject/src/stringTest/SubstringTest.java
@@ -11,7 +11,7 @@ public class SubstringTest {
 		*/
 		
 		
-		String phoneNo = "010123";
+		String phoneNo = "010123456789";
 		
 		phoneNo = phoneNo.substring(0, phoneNo.length() <= 11 ? phoneNo.length() : 11);
 		
@@ -19,18 +19,12 @@ public class SubstringTest {
 		String no2 = "";
 		String no3 = "";
 
-		if (phoneNo.length() >= 10) {
-			no1 = phoneNo.substring(0, 3);
-			no2 = phoneNo.substring(3, phoneNo.length() - 4);
-			no3 = phoneNo.substring(phoneNo.length() - 4, phoneNo.length());
-		} else if (phoneNo.length() >= 8 && phoneNo.length() <= 9) {
-			no1 = phoneNo.substring(0, 3);
+		no1 = phoneNo.substring(0, 3);
+		if (phoneNo.length() >= 8) {
 			no2 = phoneNo.substring(3, 7);
-		} else if(phoneNo.length() >= 6 && phoneNo.length() <= 7) {
-			no1 = phoneNo.substring(0, 3);
+			no3 = phoneNo.substring(7, phoneNo.length());
+		} else if (phoneNo.length() > 3 && phoneNo.length() < 8) {
 			no2 = phoneNo.substring(3, phoneNo.length());
-		} else if(phoneNo.length() >= 3) {
-			no1 = phoneNo.substring(0, 3);
 		}
 
 		System.out.println(no1);
